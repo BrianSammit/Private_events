e00713387fbb08e68b97bad069e91e71f758a426
Khristian Rojas <khristianrojas15@gmail.com>
2020-12-07 17:11:06 -0300

    Add routes to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   resources :attendances, only: [:create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'events#index'
+
+  get 'sign_up',  to:  'users#new'
+  get 'login',  to:  'sessions#new'
+  post 'login' ,  to: 'sessions#create'
+  get 'logout',  to: 'sessions#destroy'
 end
